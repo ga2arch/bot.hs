802e55f90b9796750f4208921fa943956ae5f37d
Gabriele Carrettoni <gabriele.carrettoni@satispay.com>
2017-09-08 16:09:25 +0200

    remove useless language pragmas

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,4 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DeriveFunctor, RankNTypes #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE ScopedTypeVariables, ImplicitParams #-}
 module Main where
 
 import Bot
