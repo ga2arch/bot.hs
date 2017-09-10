1bf4f1e655eef00c0797b4a715070e1c76912a20
Gabriele Carrettoni <gabriele.carrettoni@satispay.com>
2017-09-10 17:37:54 +0200

    remove new lines when reading token

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Bot
+import           Bot
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-main = TIO.readFile "token" >>= runBot
+main = TIO.readFile "token" >>= return . head . T.lines  >>= runBot
